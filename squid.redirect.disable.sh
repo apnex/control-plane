a804887b4cebbe88de8d5eb722bb6cd8b238a8f2
root <aobersnel@apnex.com.au>
2020-01-14 10:38:41 +1100

    Squid updates..

--- squid.redirect.disable.sh
+++ squid.redirect.disable.sh
@@ -5,8 +5,8 @@ IPADDR=$(ifconfig ${WANINT} | grep inet | awk '{print $2}')
 echo "WANINT: ${WANINT} | IPADDR: ${IPADDR}"
 
 ## disable squid redirect
-iptables -t nat -D SQUID -p tcp -j DNAT --to ${IPADDR}:3128
-iptables -t nat -D PREROUTING ! -i ${WANINT} -p tcp --dport 80 -j SQUID
-iptables -t nat -X SQUID
+iptables -t nat -D SQUID -p tcp -j DNAT --to ${IPADDR}:3128 2>/dev/null
+iptables -t nat -D PREROUTING ! -i ${WANINT} -p tcp --dport 80 -j SQUID 2>/dev/null
+iptables -t nat -X SQUID 2>/dev/null
 
 ./iptables.nat.list.sh
--- squid.redirect.enable.sh
+++ squid.redirect.enable.sh
@@ -4,6 +4,9 @@ WANINT=$(ip route list 0/0 | awk '{print $5}')
 IPADDR=$(ifconfig ${WANINT} | grep inet | awk '{print $2}')
 echo "WANINT: ${WANINT} | IPADDR: ${IPADDR}"
 
+## remove existing rules
+./squid.redirect.disable.sh >/dev/null
+
 ## enable squid redirect
 iptables -t nat -N SQUID
 iptables -t nat -A SQUID -p tcp -j DNAT --to ${IPADDR}:3128
