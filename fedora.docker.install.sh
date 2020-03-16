36468ac31617d0a03d8936903752d73754c08689
root <aobersnel@apnex.com.au>
2020-03-16 13:27:44 +1100

    Updates..

--- fedora.docker.install.sh
+++ fedora.docker.install.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 
 # remove existing docker
-dnf remove \
-	docker \
+dnf remove -y \
 	docker-ce \
+	docker \
 	docker-client \
 	docker-client-latest \
 	docker-common \
 	docker-latest \
 	docker-latest-logrotate \
 	docker-logrotate \
-	docker-selinux \
-	docker-engine-selinux \
 	docker-engine
 
-## add fedora repo
-dnf -y install dnf-plugins-core
+# install pre-requisites
+dnf install -y \
+	dnf-plugins-core
+
+# add docker-ce repository
 dnf config-manager \
-    --add-repo \
-    https://download.docker.com/linux/fedora/docker-ce.repo
+	--add-repo \
+	https://download.docker.com/linux/fedora/docker-ce.repo
 
 # install latest stable docker
 dnf install -y \
 	docker-ce \
 	docker-ce-cli \
-	containerd.io \
+	containerd.io
 
 # start docker and enable on-boot
 systemctl enable docker
