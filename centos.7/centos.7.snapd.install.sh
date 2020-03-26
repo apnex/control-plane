9e3572f7c3973dd28adcf02e3464251a5441d3d9
root <aobersnel@apnex.com.au>
2020-03-26 14:44:41 +1100

    Updates...

--- centos.7/centos.7.snapd.install.sh
+++ centos.7/centos.7.snapd.install.sh
@@ -1,13 +1,7 @@
-#yum install yum-plugin-copr epel-release
-#yum copr enable ngompa/snapcore-el7
-#yum install snapd
-#systemctl start snapd
-#systemctl enable --now snapd.socket
-#export PATH=$PATH:/snap/bin
-#snap alias microk8s.kubectl kubectl
-
+#!/bin/bash
 yum install epel-release
 yum install snapd
 systemctl start snapd
 systemctl enable --now snapd.socket
-
+ln -s /var/lib/snapd/snap /snap
+exec bash
