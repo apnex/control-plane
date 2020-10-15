866be67173deee2b63f51c2449d23c7a50f91b00
root <aobersnel@apnex.com.au>
2020-10-15 13:44:59 +1100

    Updates..

--- squid.start.sh
+++ squid.start.sh
@@ -5,15 +5,21 @@ if [ ! -d ./cache ]; then
 	mkdir cache
 fi
 
+## build auth file.
+printf "buster:$(openssl passwd -crypt 'test')\n" | sudo tee -a ${PWD}/htpasswd
+
 # launch & persist
 docker rm -v $(docker ps -qa -f name=squid -f status=exited) 2>/dev/null
 RUNNING=$(docker ps -q -f name="squid")
 if [[ -z "$RUNNING" ]]; then
 	printf "[apnex/squid] not running - now starting\n" 1>&2
-	docker run --name squid -d -p 3128:3128 \
+	# -p 3128:3128
+	docker run --name squid -d --net host \
 		-v ${PWD}/squid.entrypoint.sh:/sbin/entrypoint.sh \
-		-v ${PWD}/squid.conf:/etc/squid3/squid.conf \
+		-v ${PWD}/squid.auth:/etc/squid3/squid.conf \
+		-v ${PWD}/htpasswd:/etc/squid3/htpasswd \
 		-v ${PWD}/cache:/var/spool/squid3 \
 	apnex/control-squid
-	./squid.redirect.enable.sh
+	#./squid.redirect.enable.sh
 fi
+
